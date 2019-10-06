b52ee55ae842dc3f29edf6632d1727bb4ba575cc
Goran Tasev <gtasev983@gmail.com>
2019-10-06 17:37:51 +0200

    starting with app

--- project/src/App.js
+++ project/src/App.js
@@ -1,21 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './person/person';
 
-function App() {
-  var app =(
-      <div>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29" />
-      </div>
-  )
-
-    return(
-      <div>
-          {app}
-      </div>
-    );
+class App extends Component {
+  render() {
+      return(
+          <div className="App">
+              <h1>Hi, I'm a aReact App</h1>
+          </div>
+      );
+  }
 }
 
 
