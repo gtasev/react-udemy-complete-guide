5dff563034ccd3debc0525343ae5303d8720020d
gtasev <gtasev983@gmail.com>
2019-10-08 12:38:44 +0200

    module 4 done

--- project/src/App61.js
+++ project/src/App61.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react'
 
 import ValidationCom from './ValidationComponent/ValidationComponent'
+import Char from './Char/Char'
 
 class App61 extends Component {
     state = {
-        text: '',
-        textLength: 0
+        userInput: '',
     };
 
-    mergeAndAssignText = (char) => {
-      const newState = {...this.state};
-        newState.text = char;
-        newState.textLength = newState.text.length;
-        this.setState(newState);
-        console.log(this.state);
+    inputChangedHandler = (event) => {
+        this.setState({userInput: event.target.value})
     };
 
-
+    handleRemove = (index) => {
+        let editedUserInput = {...this.state};
+        editedUserInput.userInput = editedUserInput.userInput.slice(0, index) + editedUserInput.userInput.slice(index + 1);
+        this.setState(editedUserInput);
+    };
 
     render() {
+        let charList = null;
+
+        if (this.state.userInput.length > 0) {
+           charList = this.state.userInput.split('').map((char, index) => {
+               return <Char
+                   key={index}
+                   character={char}
+                   removeChar={() => this.handleRemove(index)}
+               />
+           })
+        }
+
         return(
             <div>
-                <input type="text" value={this.state.text} onChange={(event) => {this.mergeAndAssignText(event.target.value)}} />
-                <ValidationCom textLength={this.state.textLength}/>
+                <input type="text"
+                       value={this.state.userInput}
+                       onChange={this.inputChangedHandler}
+                />
+                <p>{this.state.userInput}</p>
+                <ValidationCom inputLength={this.state.userInput.length}/>
+                {charList}
             </div>
 
         )
--- project/src/ValidationComponent/ValidationComponent.js
+++ project/src/ValidationComponent/ValidationComponent.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const validationComponent = (props) => {
 
-    const message = props.textLength <= 5 ? 'Text too short!' : 'Text long enough!';
+    const message = props.inputLength < 5 ? 'Text too short!' : 'Text long enough!';
 
     return (
-        <p>{message}</p>
+        <div>
+            <p>{message}</p>
+        </div>
+
     )
 };
 
